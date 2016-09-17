cc43728a229dba4c63ee663388f16a6f7cabd1df
Henry Stern <hankeyjunk@gmail.com>
2016-09-17 14:45:07 -0400

    Updated for Swift 3

--- CreatePDF.swift
+++ CreatePDF.swift
@@ -2,20 +2,20 @@ import Cocoa
 import WebKit
 
 public func CreatePDF(htmlString: String) {
-   var webView = WebView()
-   webView.mainFrame.loadHTMLString(htmlString, baseURL: nil)
-   let when = DispatchTime.now() + 1
-   DispatchQueue.main.asyncAfter(deadline: when) {
-   let printOpts: [String : AnyObject] = [NSPrintJobDisposition:NSPrintSaveJob]
-   let printInfo: NSPrintInfo = NSPrintInfo(dictionary: printOpts)
-   printInfo.paperSize = NSMakeSize(595.22, 841.85)
-   printInfo.topMargin = 10.0
-   printInfo.leftMargin = 10.0
-   printInfo.rightMargin = 10.0
-   printInfo.bottomMargin = 10.0
-   let printOp: NSPrintOperation = NSPrintOperation(view: self.webView.mainFrame.frameView.documentView, printInfo: printInfo)
-   printOp.showsPrintPanel = false
-   printOp.showsProgressPanel = false
-   printOp.runOperation()
-}
+    let webView = WebView()
+    webView.mainFrame.loadHTMLString(htmlString, baseURL: nil)
+    let when = DispatchTime.now() + 1
+    DispatchQueue.main.asyncAfter(deadline: when) {
+        let printOpts: [String : AnyObject] = [NSPrintJobDisposition:NSPrintSaveJob]
+        let printInfo: NSPrintInfo = NSPrintInfo(dictionary: printOpts)
+        printInfo.paperSize = NSMakeSize(595.22, 841.85)
+        printInfo.topMargin = 10.0
+        printInfo.leftMargin = 10.0
+        printInfo.rightMargin = 10.0
+        printInfo.bottomMargin = 10.0
+        let printOp: NSPrintOperation = NSPrintOperation(view: webView.mainFrame.frameView.documentView, printInfo: printInfo)
+        printOp.showsPrintPanel = false
+        printOp.showsProgressPanel = false
+        printOp.run()
+    }
 }
